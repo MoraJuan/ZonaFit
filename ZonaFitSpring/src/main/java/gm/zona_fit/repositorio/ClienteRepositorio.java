f0644917d737d83c8726605fed9a94596c68f30d
Juan Cruz Mora <73558008+MoraJuan@users.noreply.github.com>
2024-08-29 13:30:02 -0300

    Delete ZonaFitSpring directory

--- ZonaFitSpring/src/main/java/gm/zona_fit/repositorio/ClienteRepositorio.java
+++
@@ -1,7 +0,0 @@
-package gm.zona_fit.repositorio;
-
-import gm.zona_fit.modelo.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepositorio extends JpaRepository<Cliente,Integer> {
-}
